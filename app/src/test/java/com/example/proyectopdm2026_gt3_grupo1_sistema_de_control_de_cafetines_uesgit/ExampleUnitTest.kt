36f64978f99005a77ec9f013fdb462db47b3b9e7
Erick2517 <ericklandaverde15@gmail.com>
2026-04-07 21:21:03 -0600

    commit inicial

---
+++ app/src/test/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.example.proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
