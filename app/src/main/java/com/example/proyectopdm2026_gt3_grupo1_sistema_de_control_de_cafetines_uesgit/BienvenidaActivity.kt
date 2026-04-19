ecc09466d003c5f1bfb81c1ce21302ae27fc230c
Erick2517 <ericklandaverde15@gmail.com>
2026-04-19 15:02:46 -0600

    se agrega pantalla BienvenidaActivity

--- app/src/main/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/BienvenidaActivity.kt
+++ app/src/main/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/BienvenidaActivity.kt
@@ -1,34 +1,20 @@
 package com.example.proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class BienvenidaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
-        btnLogin.setOnClickListener {
-            val intent = Intent(
-                this,
-                BienvenidaActivity::class.java
-            )
-            startActivity(intent)
-            finish()
-        }
-
+        setContentView(R.layout.activity_bienvenida)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
     }
 }
