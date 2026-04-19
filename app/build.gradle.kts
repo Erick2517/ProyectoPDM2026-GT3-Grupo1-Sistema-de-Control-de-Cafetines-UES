a56f15e1549516c51a8f74dc1890fdb13eee09de
Erick2517 <ericklandaverde15@gmail.com>
2026-04-19 16:12:44 -0600

    se agrega pantalla LocalesActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //agregado
+    implementation(libs.cardview)
 }
--- app/src/main/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/LocalesActivity.kt
+++ app/src/main/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/LocalesActivity.kt
@@ -1,38 +1,21 @@
 package com.example.proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class BienvenidaActivity : AppCompatActivity() {
+class LocalesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_bienvenida)
+        setContentView(R.layout.activity_locales)
 
-        val btnLogOut = findViewById<Button>(R.id.btnCerrarSesion)
-        val btnVerLocales = findViewById<Button>(R.id.btnVerLocales)
-
-        btnLogOut.setOnClickListener {
-            val intent = Intent(
-                this,
-                LoginActivity::class.java
-            )
-            startActivity(intent)
+        val btnBack = findViewById<ImageView>(R.id.btnBack)
+        btnBack.setOnClickListener {
             finish()
-        }
-
-        btnVerLocales.setOnClickListener {
-            val intent = Intent(
-                this,
-                LocalesActivity::class.java
-            )
-            startActivity(intent)
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
