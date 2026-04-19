f46858abbd5e007e0c2636fb2897fa3c7eb25d27
Erick2517 <ericklandaverde15@gmail.com>
2026-04-19 15:37:33 -0600

    se agrega pantalla RegistroActivity y navegacón entre pantallas existentes

--- app/src/main/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/LoginActivity.kt
+++ app/src/main/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.example.proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_u
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,10 +16,21 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val lblRegistrarse = findViewById<TextView>(R.id.lbl_registrarse);
+
         btnLogin.setOnClickListener {
             val intent = Intent(
                 this,
                 BienvenidaActivity::class.java
+            )
+            startActivity(intent)
+            finish()
+        }
+
+        lblRegistrarse.setOnClickListener {
+            val intent = Intent(
+                this,
+                RegistroActivity::class.java
             )
             startActivity(intent)
             finish()
--- app/src/main/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/RegistroActivity.kt
+++ app/src/main/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/RegistroActivity.kt
@@ -9,14 +9,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class BienvenidaActivity : AppCompatActivity() {
+class RegistroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_bienvenida)
+        setContentView(R.layout.activity_registro)
 
-        val btnLogOut = findViewById<Button>(R.id.btnCerrarSesion)
-        btnLogOut.setOnClickListener {
+        val lblLogin = findViewById<TextView>(R.id.lbl_login);
+        val btnRegistrar = findViewById<Button>(R.id.btnRegistrar);
+
+        lblLogin.setOnClickListener {
             val intent = Intent(
                 this,
                 LoginActivity::class.java
@@ -24,6 +26,16 @@ class BienvenidaActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        btnRegistrar.setOnClickListener {
+            val intent = Intent(
+                this,
+                BienvenidaActivity::class.java
+            )
+            startActivity(intent);
+            finish();
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
