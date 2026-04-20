4fdff486d9459a84f4b2ccde0d6b44b4c3b99360
Erick2517 <ericklandaverde15@gmail.com>
2026-04-19 20:45:03 -0600

    se agrega Pantalla Nuevo Producto

--- app/src/main/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/NuevoProductoActivity.kt
+++ app/src/main/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/NuevoProductoActivity.kt
@@ -9,26 +9,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class GestionarProductos : AppCompatActivity() {
+class NuevoProductoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_gestionar_productos)
+        setContentView(R.layout.activity_nuevo_producto)
 
         val btnBack = findViewById<ImageView>(R.id.btnBack)
         btnBack.setOnClickListener {
             finish()
         }
 
-        val btnAgregarProd = findViewById<Button>(R.id.btnAgregarProducto)
-        btnAgregarProd.setOnClickListener {
+        val btnAddLocal = findViewById<Button>(R.id.btnGuardar)
+        btnAddLocal.setOnClickListener {
             val intent = Intent(
                 this,
-                NuevoProductoActivity::class.java
+                NuevoLocalActivity::class.java
             )
             startActivity(intent)
+            finish()
         }
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
