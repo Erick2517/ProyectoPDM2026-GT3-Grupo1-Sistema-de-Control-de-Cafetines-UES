51a5898b3db6d456247cbb7c1105e88bc5fc3192
Erick2517 <ericklandaverde15@gmail.com>
2026-04-19 18:01:18 -0600

    se agrega pantalla ConfirmarPagoActivity.kt

--- app/src/main/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/ConfirmarPagoActivity.kt
+++ app/src/main/java/com/example/proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_uesgit/ConfirmarPagoActivity.kt
@@ -3,32 +3,37 @@ package com.example.proyectopdm2026_gt3_grupo1_sistema_de_control_de_cafetines_u
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class PagoActivity : AppCompatActivity() {
+class ConfirmarPagoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_pago)
+        setContentView(R.layout.activity_confirmar_pago)
 
-        val btnBack = findViewById<ImageView>(R.id.btnBack)
-        val btnConfirmar = findViewById<Button>(R.id.btnConfirmar)
+        val btnVerPedidos = findViewById<Button>(R.id.btnVerPedidos)
+        val lblRegresarInicio = findViewById<TextView>(R.id.lblVolverInicio);
 
-        btnBack.setOnClickListener {
-            finish()
-        }
-
-        btnConfirmar.setOnClickListener {
+        /*btnVerPedidos.setOnClickListener {
             val intent = Intent(
                 this,
-                ConfirmarPagoActivity::class.java
+                BienvenidaActivity::class.java
             )
             startActivity(intent)
-            finish();
+            finish()
+        }*/
+
+        lblRegresarInicio.setOnClickListener {
+            val intent = Intent(
+                this,
+                BienvenidaActivity::class.java
+            )
+            startActivity(intent)
+            finish()
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
